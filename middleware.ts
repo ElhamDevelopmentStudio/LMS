c545320379444232e826bea1034b516fbc3cfce3
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-04-22 00:46:48 +0430

    added the main sidebar to the dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({ authorizedParties: ["/test"] });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
