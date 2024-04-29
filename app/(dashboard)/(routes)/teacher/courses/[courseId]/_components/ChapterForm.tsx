64b51758d3777754c8bce1188031314c1f87108c
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-04-30 01:19:52 +0430

    Modified chapter form and created a chapter list as well as activated drag and drop feature

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChapterForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChapterForm.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
@@ -34,7 +34,7 @@ const formSchema = z.object({
 
 export const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
   const [isCreating, setIsCreating] = useState(false);
-  const [isUpdateing, setIsUpdating] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
 
   const toggleCreating = () => {
     setIsCreating((current) => !current);
@@ -62,8 +62,32 @@ export const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
     }
   };
 
+  const onReOrder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast.success("Chapters reorderd.");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong.");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -115,8 +139,8 @@ export const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
         >
           {!initialData.chapters.length && "No Chapters"}
           <ChaptersList
-            onEdit={() => {}}
-            onReorder={() => {}}
+            onEdit={onEdit}
+            onReorder={onReOrder}
             items={initialData.chapters || []}
           />
         </div>
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersList.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersList.tsx
@@ -11,7 +11,8 @@ import {
 } from "@hello-pangea/dnd";
 
 import { cn } from "@/lib/utils";
-import { Grip } from "lucide-react";
+import { Grip, Pencil } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 interface ChaptersListProps {
   items: Chapter[];
@@ -35,12 +36,34 @@ export const ChaptersList = ({
     setChapters(items);
   }, [items]);
 
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const item = Array.from(chapters);
+    const [reOrderedItems] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reOrderedItems);
+
+    const startIndex = Math.min(result.source.index, result.destination.index);
+    const endIndex = Math.max(result.source.index, result.destination.index);
+
+    const updatedChapters = items.slice(startIndex, endIndex + 1);
+
+    setChapters(items);
+
+    const bulkUpdateData = updatedChapters.map((chapter) => ({
+      id: chapter.id,
+      position: items.findIndex((item) => item.id === chapter.id),
+    }));
+
+    onReorder(bulkUpdateData);
+  };
+
   if (!isMounted) {
     return null;
   }
 
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="chapters">
         {(provided) => (
           <div {...provided.droppableProps} ref={provided.innerRef}>
@@ -71,10 +94,26 @@ export const ChaptersList = ({
                       <Grip className="h-5 w-5" />
                     </div>
                     {chapter.title}
+                    <div className="ml-auto pr-1  items-center gap-x-2">
+                      {chapter.isFree && <Badge>Free</Badge>}
+                      <Badge
+                        className={cn(
+                          "bg-slate-500",
+                          chapter.isPublished && "bg-sky-700"
+                        )}
+                      >
+                        {chapter.isPublished ? "Published" : "Draft"}
+                      </Badge>
+                    </div>
+                    <Pencil
+                      onClick={() => onEdit(chapter.id)}
+                      className="w-4 h-4 mr-2 cursor-pointer hover:opacity-75 transition"
+                    />
                   </div>
                 )}
               </Draggable>
             ))}
+            {provided.placeholder}
           </div>
         )}
       </Droppable>
