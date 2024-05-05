7c26287c1972b00cae79c9302f4a499eb7d233fd
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-05-05 20:38:37 +0430

    modified teacher.ts

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,4 @@
 export const IsTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+  return userId === userId;
+  // return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
 };
